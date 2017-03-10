86ffe604c50601810ef1a29c3c69307746863c78
pgaines937 <pgaines937@gmail.com>
2017-03-10 08:22:11 -0700

    Initial commit

---
+++ news_articles/spiders/nasdaq_spider.py
@@ -0,0 +1,27 @@
+import scrapy
+import time
+import datetime
+
+"""nasdaq_spider.py
+
+run with: scrapy runspider nasdaq_spider.py -o nasdaq.jl"""
+
+
+class NasdaqSpider(scrapy.Spider):
+    name = "nasdaq google news headlines"
+    start_urls = [
+        'http://www.nasdaq.com/symbol/goog/news-headlines',
+    ]
+
+    def parse(self, response):
+        for news_headline in response.css('div.news-headlines'):
+            yield {
+                'text': news_headline.xpath('div/span/a/text()').extract(),
+                'url': news_headline.xpath('div/span/a/@href').extract(),
+                'timestamp': news_headline.xpath('div/small/text()').extract()
+            }
+
+        next_page = response.css('li.quotes_content_left_lb_NextPage a::attr("href")').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
