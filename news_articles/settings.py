aba9ec49815c8d34d5b50220c9461a5ee5caef1d
pgaines937 <pgaines937@gmail.com>
2017-03-10 09:48:09 -0700

    Change pipeline to MongoDBPipeline

--- news_articles/settings.py
+++ news_articles/settings.py
@@ -65,7 +65,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'news_articles.pipelines.DefaultPipeline': 300,
+#    'news_articles.pipelines.MongoDBPipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
