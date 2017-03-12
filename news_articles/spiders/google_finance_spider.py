1190205a476e8c55547e0737b22d90b72b8e717a
pgaines937 <pgaines937@gmail.com>
2017-03-12 16:26:43 -0700

    .

--- news_articles/spiders/google_finance_spider.py
+++ news_articles/spiders/google_finance_spider.py
@@ -5,6 +5,7 @@ import datetime
 from scrapy.spiders import CrawlSpider
 from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
+from scrapy.utils.response import get_base_url
 
 """google_finance_spider.py
 
@@ -18,9 +19,9 @@ class GoogleFinanceSpider(CrawlSpider):
         'https://www.google.com/finance/company_news?q=NASDAQ%3AGOOG&startdate=2017-1-01&enddate=2017-2-01&ei=-8zFWLnSFoe62Aam_4XoCQ',
     ]
 
-    rules = (
-        Rule(LinkExtractor(allow=(), restrict_xpaths=('.//td[@class="nav_b"]/a/@href',)), callback="parse", follow=True),
-    )
+    #rules = (
+     #   Rule(LinkExtractor(allow=(), restrict_xpaths=('.//td[@class="nav_b"]/a/@href',)), callback="parse", follow=True),
+    #)
 
     def parse(self, response):
         for news_headline in response.css('div.news'):
@@ -29,3 +30,8 @@ class GoogleFinanceSpider(CrawlSpider):
                 'url': news_headline.css('span.name').xpath('a/@href').extract(),
                 'date': news_headline.css('span.date').xpath('text()').extract()
             }
+
+        next_page = response.xpath('.//td[@class="nav_b"]/a/@href').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
