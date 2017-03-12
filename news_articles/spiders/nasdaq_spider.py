6c0128ce58bb9dbbb4d9031ac70f867edbc0415a
pgaines937 <pgaines937@gmail.com>
2017-03-11 17:36:09 -0700

    Editing Nasdaq Spider

--- news_articles/spiders/nasdaq_spider.py
+++ news_articles/spiders/nasdaq_spider.py
@@ -17,9 +17,9 @@ class NasdaqSpider(scrapy.Spider):
     def parse(self, response):
         for news_headline in response.css('div.news-headlines'):
             yield {
-                'headline': news_headline.xpath('div/span/a/text()').extract(),
-                'url': news_headline.xpath('div/span/a/@href').extract(),
-                'timestamp': news_headline.xpath('normalize-space(div/small/text())').extract()
+                'headline': news_headline.xpath('div.news-headlines/div/span/a/text()').extract(),
+                'url': news_headline.xpath('div.news-headlines/div/span/a/@href').extract(),
+                'timestamp': news_headline.xpath('div.news-headlines/div/small/text()').extract()
             }
 
         next_page = response.css('li.quotes_content_left_lb_NextPage a::attr("href")').extract_first()
