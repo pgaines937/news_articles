42f65b5eb9b1649d13eced83d10e9365bb77c651
pgaines937 <pgaines937@gmail.com>
2017-03-12 11:55:12 -0700

    .

--- news_articles/settings.py
+++ news_articles/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'news_articles.spiders'
 USER_AGENT = 'news_articles (+http://www.pgaines937.io)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
