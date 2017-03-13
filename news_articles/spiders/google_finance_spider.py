12ba541a74cd96181a9166ac4cfc58ac79570785
pgaines937 <pgaines937@gmail.com>
2017-03-12 17:26:22 -0700

    Another try to get google finance spider working

--- news_articles/spiders/google_finance_spider.py
+++ news_articles/spiders/google_finance_spider.py
@@ -14,16 +14,16 @@ run with: scrapy runspider google_finance_spider.py -o google_finance.jl
             scrapy crawl google_finance -o google_finance.json"""
 
 
-class GoogleFinanceSpider(CrawlSpider):
+class GoogleFinanceSpider(scrapy.Spider):
     name = "google_finance"
     allowed_domains = ["www.google.com"]
     start_urls = [
         'https://www.google.com/finance/company_news?q=NASDAQ%3AGOOG&startdate=2017-1-01&enddate=2017-2-01&ei=-8zFWLnSFoe62Aam_4XoCQ',
     ]
 
-    rules = (
-        Rule(SgmlLinkExtractor(allow=(), restrict_xpaths=('.//td[@class="nav_b"]/a/@href',)), callback="parse", follow=True),
-    )
+    #rules = (
+    #    Rule(SgmlLinkExtractor(allow=(), restrict_xpaths=('.//td[@class="nav_b"]/a/@href',)), callback="parse", follow=True),
+    #)
 
     def parse(self, response):
         for news_headline in response.css('div.news'):
