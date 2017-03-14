0c2cf46afd5ad8d5128e45696b3ebf36d49314c5
pgaines937 <pgaines937@gmail.com>
2017-03-13 20:07:03 -0700

    added url parsing for google news links

--- news_articles/items.py
+++ news_articles/items.py
@@ -8,12 +8,19 @@
 import scrapy
 from scrapy.loader.processors import MapCompose, Join
 from scrapy.utils.markup import replace_escape_chars
-import time
-import datetime
+
+import w3lib.url
 
 class NewsArticle(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    headline = scrapy.Field()
+    url = scrapy.Field(
+        input_processor=MapCompose(
+            lambda x: [w3lib.url.url_query_parameter(u, "url") for u in x]),
+        output_processor=Join()
+    )
+    date = scrapy.Field()
     headline = scrapy.Field()
     url = scrapy.Field()
     date = scrapy.Field()
