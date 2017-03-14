c2bccdd6abb85d12ecb8096a34807c4331b8777f
pgaines937 <pgaines937@gmail.com>
2017-03-13 17:48:11 -0700

    Removed imports and commented code

--- news_articles/spiders/google_finance_spider.py
+++ news_articles/spiders/google_finance_spider.py
@@ -1,18 +1,9 @@
 import scrapy
-import time
-import datetime
-
-from scrapy.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.spiders import CrawlSpider
-from scrapy.spiders import Rule
-from scrapy.linkextractors import LinkExtractor
-from scrapy.utils.response import get_base_url
 
 """google_finance_spider.py
 
 run with: scrapy runspider google_finance_spider.py -o google_finance.jl
             scrapy crawl google_finance -o google_finance.json"""
-
 
 class GoogleFinanceSpider(scrapy.Spider):
     name = "google_finance"
@@ -21,10 +12,6 @@ class GoogleFinanceSpider(scrapy.Spider):
         'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&ei=jufFWMGjCYayjAG925mYDA&startdate=2017-01-01&enddate=2017-02-01&start=0&num=100',
     ]
 
-    #rules = (
-    #    Rule(SgmlLinkExtractor(allow=(), restrict_xpaths=('.//td[@class="nav_b"]/a/@href',)), callback="parse", follow=True),
-    #)
-
     def parse(self, response):
         for news_headline in response.css('div.news'):
             yield {
@@ -32,9 +19,3 @@ class GoogleFinanceSpider(scrapy.Spider):
                 'url': news_headline.css('span.name').xpath('a/@href').extract(),
                 'date': news_headline.css('span.date').xpath('text()').extract()
             }
-
-        #next_page = response.xpath('.//td[@class="nav_b"]/a/@href').extract_first()
-        #next_page = response.css('td.nav_b a::attr("href")').extract_first()
-        #if next_page is not None:
-        #    next_page = response.urljoin(next_page)
-        #    yield scrapy.Request(next_page, callback=self.parse)
