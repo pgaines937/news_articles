302a9314a7221731103bd1815ec358a08edc9624
pgaines937 <pgaines937@gmail.com>
2017-03-19 15:05:42 -0700

    .

--- news_articles/data/post_process_data.py
+++ news_articles/data/post_process_data.py
@@ -49,9 +49,8 @@ def flatten_articles():
     try:
         article_data = {}
         for item in database.articles.find():
-            if item['url'] is None:
-                break
-            print(item)
+            if item['url'] is not None:
+                print(item)
     except Exception as e:
         print("Error: " + str(e))
 
