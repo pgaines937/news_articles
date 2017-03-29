79ebdcbfb6cd72420d4c52b568884dcccc7283f8
pgaines937 <pgaines937@gmail.com>
2017-03-29 13:47:53 -0700

    .

--- news_articles/spiders/google_finance_spider.py
+++ news_articles/spiders/google_finance_spider.py
@@ -11,7 +11,7 @@ after the spider runs, export the database:
 class GoogleFinanceSpider(scrapy.Spider):
     name = "google_finance"
     allowed_domains = ["www.google.com"]
-    start_urls = [
+    start_urls = ["""
         'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&ei=jufFWMGjCYayjAG925mYDA&startdate=2016-12-01&enddate=2017-01-01&start=0&num=100',
         'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&ei=jufFWMGjCYayjAG925mYDA&startdate=2017-01-01&enddate=2017-02-01&start=0&num=100',
         'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&ei=jufFWMGjCYayjAG925mYDA&startdate=2017-02-01&enddate=2017-03-01&start=0&num=100',
@@ -22,7 +22,8 @@ class GoogleFinanceSpider(scrapy.Spider):
         'https://www.google.com/search?q=google&biw=1920&bih=950&tbm=nws&ei=BxfcWP7PJMme0gKQ5rKwCQ&start=300&num=100'
         'https://www.google.com/search?q=google&biw=1920&bih=950&tbm=nws&ei=BxfcWP7PJMme0gKQ5rKwCQ&start=400&num=100'
         'https://www.google.com/search?q=google&biw=1920&bih=950&tbm=nws&ei=BxfcWP7PJMme0gKQ5rKwCQ&start=500&num=100'
-
+"""
+        'https://www.google.com/search?q=google&biw=1920&bih=950&tbm=nws&start=0&num=100'
     ]
 
     def parse(self, response):
