f439e5d22539577a9336dfa6c6e0224cae4df9d4
pgaines937 <pgaines937@gmail.com>
2017-03-29 14:57:24 -0700

    .

--- news_articles/spiders/google_finance_spider.py
+++ news_articles/spiders/google_finance_spider.py
@@ -12,12 +12,15 @@ class GoogleFinanceSpider(scrapy.Spider):
     name = "google_finance"
     allowed_domains = ["www.google.com"]
     start_urls = [
-        'http://www.nasdaq.com/symbol/goog/news-headlines'
+        'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&ei=jufFWMGjCYayjAG925mYDA&startdate=2016-12-01&enddate=2017-01-01&start=0&num=100',
+        'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&ei=jufFWMGjCYayjAG925mYDA&startdate=2017-01-01&enddate=2017-02-01&start=0&num=100',
+        'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&ei=jufFWMGjCYayjAG925mYDA&startdate=2017-02-01&enddate=2017-03-01&start=0&num=100',
+        'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&ei=jufFWMGjCYayjAG925mYDA&startdate=2017-03-01&enddate=2017-04-01&start=0&num=100'
     ]
 
     def parse(self, response):
         for news_headline in response.css('div.news'):
             yield {
-                'headline_text': news_headline.css('a').xpath('text()').extract(),
-                'url': news_headline.css('a').xpath('@href').extract()
+                'headline_text': news_headline.css('a#n-cn-').xpath('text()').extract(),
+                'url': news_headline.css('a#n-cn-').xpath('@href').extract()
             }
--- news_articles/spiders/nasdaq_spider.py
+++ news_articles/spiders/nasdaq_spider.py
@@ -12,17 +12,59 @@ class NasdaqSpider(scrapy.Spider):
     name = "nasdaq"
     start_urls = [
         'http://www.nasdaq.com/symbol/goog/news-headlines',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=2',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=3',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=4',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=5',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=6',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=7',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=8',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=9',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=10',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=11',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=12',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=13',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=14',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=15',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=16',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=17',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=18',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=19',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=20',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=21',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=22',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=23',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=24',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=25',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=26',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=27',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=28',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=29',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=30',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=31',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=32',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=33',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=34',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=35',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=36',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=37',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=38',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=39',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=40',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=41',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=42',
+        'http://www.nasdaq.com/symbol/goog/news-headlines?page=43'
     ]
 
     def parse(self, response):
         for news_headline in response.css('div.news-headlines'):
             yield {
                 'headline': news_headline.xpath('div/span/a/text()').extract(),
-                'url': news_headline.xpath('div/span/a/@href').extract(),
-                'timestamp': news_headline.xpath('div/small/text()').extract()
+                'url': news_headline.xpath('div/span/a/@href').extract()
+                #'timestamp': news_headline.xpath('div/small/text()').extract()
             }
 
-        next_page = response.css('li.quotes_content_left_lb_NextPage a::attr("href")').extract_first()
+        """next_page = response.css('li.quotes_content_left_lb_NextPage a::attr("href")').extract_first()
         if next_page is not None:
             next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+            yield scrapy.Request(next_page, callback=self.parse)"""
