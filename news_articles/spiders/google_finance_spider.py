171b3ec7fe6524fe448a6c94adcffe02f39b13d4
pgaines937 <pgaines937@gmail.com>
2017-03-29 21:50:25 -0700

    .

--- news_articles/spiders/google_finance_spider.py
+++ news_articles/spiders/google_finance_spider.py
@@ -13,10 +13,10 @@ after the spider runs, export the database:
 class GoogleFinanceSpider(scrapy.Spider):
     name = "google_finance"
     start_urls = [
-        'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&ei=jufFWMGjCYayjAG925mYDA&startdate=2016-12-01&enddate=2017-01-01&start=0&num=100',
-        'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&ei=jufFWMGjCYayjAG925mYDA&startdate=2017-01-01&enddate=2017-02-01&start=0&num=100',
-        'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&ei=jufFWMGjCYayjAG925mYDA&startdate=2017-02-01&enddate=2017-03-01&start=0&num=100',
-        'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&ei=jufFWMGjCYayjAG925mYDA&startdate=2017-03-01&enddate=2017-04-01&start=0&num=100'
+        'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&startdate=2016-12-01&enddate=2017-01-01&start=0&num=100',
+        'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&startdate=2017-01-01&enddate=2017-02-01&start=0&num=100',
+        'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&startdate=2017-02-01&enddate=2017-03-01&start=0&num=100',
+        'https://www.google.com/finance/company_news?q=NASDAQ:GOOG&sstartdate=2017-03-01&enddate=2017-04-01&start=0&num=100'
     ]
 
     def parse(self, response):
